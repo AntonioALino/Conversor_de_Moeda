204464e664e6cb1e21cd5fe0cb7022770c4b8e02
Lino <antonioaplino@gmail.com>
2023-07-25 23:03:45 -0300

    Criando um sistema de adicionar moedas pelo usuário

--- Conversor_De_Moedas/Program.cs
+++ Conversor_De_Moedas/Program.cs
@@ -1,44 +1,112 @@
-﻿public class Conversor
+﻿using System;
+using System.Collections.Generic;
+public class Conversor
 {
     static void Main(string[] args)
     {
-        int opcao;
+        
+        
+        
 
-        int dinheiro;
-        float euro = 5.25f;
-        float dolar = 4.75f;
-        float dolarCand = 3.61f;
-        float libra = 6.13f;
+        int orçamento;
+        string opcao;
+     
+        Console.WriteLine("Você deseja fazer orçamento?");
+        opcao = Console.ReadLine(); 
+        
+        if (opcao == "sim") {
 
-        Console.WriteLine("Quanto de dinheiro você pretende investir nessa viagem??");
-        dinheiro = int.Parse(Console.ReadLine());
+        Dinheiro();
 
-        switch (dinheiro) {
-            case 0:
-                if(opcao == euro)
+        DigiteOpcoes();
+
+        
+
+           
+
+        } else
+        {
+            Console.WriteLine("Muito obrigado!");
+        } 
+
+
+
+       
+
+               
+       
+    }
+    static void DigiteOpcoes()
+    {
+        
+        List<object> moedas = new List<object>();
+
+        moedas.Add(ValordasMoedas());
+
+
+        Console.WriteLine("Digite 1 para fazer orçamento com euro");
+        Console.WriteLine("Digite 2 para fazer orçamento com dolar");
+        Console.WriteLine("Digite 3 para fazer orçamento com dolar canadense");
+        Console.WriteLine("Digite 4 para fazer orçamento com libra");
+        Console.WriteLine("Digite 5 para registrar uma moeda e seu valor");
+
+        Console.Write("Digite a opcao que voce deseja escolher\n");
+        int opcaoEscolhida = int.Parse(Console.ReadLine());
+
+        switch(opcaoEscolhida)
+        {
+            case 1:
+                if (opcaoEscolhida== 1) {
+                    Console.WriteLine(Dinheiro() * euro);
+                } break;
+                case 2:
+                if (opcaoEscolhida == 2)
                 {
-                    Console.WriteLine(dinheiro * euro);
-                }
-                break; case 1:
-                if (opcao == euro)
-                {
-                    Console.WriteLine(dinheiro * euro);
+                    Console.WriteLine(Dinheiro() * dolar);
                 }
                 break;
-            case 2:
-                if (opcao == euro)
+                case 3:
+                if (opcaoEscolhida == 3)
                 {
-                    Console.WriteLine(dinheiro * euro);
+                    Console.WriteLine(Dinheiro() * dolarCand);
                 }
+                break;
+                case 4:
+                if (opcaoEscolhida == 4)
+                {
+                    Console.WriteLine(Dinheiro() * libra);
+                }
+                break;
+                case 5: RegistrarMoeda();
+                break;
+            default: Console.WriteLine("Não existe essa função.");
                 break;
         }
     }
-    void opcao()
+    static void RegistrarMoeda()
     {
+
+    Console.Clear();
+    Console.WriteLine("Digite o nome da moeda que você deseja adicionar");
+    string nomeMoeda = Console.ReadLine();
+
+        Console.WriteLine("Digite o valor da moeda em relação a reais.");
+        float valorMoeda = float.Parse(Console.ReadLine());
+
 
     }
 
+    static int Dinheiro()
+    {
+        int dinheiro;
+        Console.WriteLine("Quanto de dinheiro você pretende investir nessa viagem??");
+        dinheiro = int.Parse(Console.ReadLine());
 
-
-}
+        return dinheiro;
+    }
+    static void ValorDasMoedas()
+    {
+        float euro =  
+    } 
+}   
 
